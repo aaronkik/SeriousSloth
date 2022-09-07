4c8b9bd49a6185056b24c7436db92d99a9a773bf
Aaron <aaronkikabhai97@gmail.com>
2022-09-07 10:20:15 +0100

    Add Roboto font

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      <link
+        href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
+        rel='stylesheet'
+      />
+    </Head>
+    <body className='bg-neutral-900 text-neutral-100'>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [require('@tailwindcss/forms')],
 };
