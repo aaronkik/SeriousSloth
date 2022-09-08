83fb70f56f2ee5aa1059ac59236b7f782d12c104
Aaron <aaronkikabhai97@gmail.com>
2022-09-08 10:54:38 +0100

    Add padding to container

--- components/shared/container.tsx
+++ components/shared/container.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 const Container = ({ className, ...props }: Props) => (
-  <div className={twMerge('mx-auto max-w-6xl', className)} {...props} />
+  <div className={twMerge('mx-auto max-w-6xl px-4', className)} {...props} />
 );
 
 export default Container;
