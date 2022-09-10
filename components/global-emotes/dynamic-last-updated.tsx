fe973a81972f66bec965e854531d359301f17227
Aaron <aaronkikabhai97@gmail.com>
2022-09-10 15:43:26 +0100

    Update dynamic updated component

--- components/global-emotes/dynamic-last-updated.tsx
+++ components/global-emotes/dynamic-last-updated.tsx
@@ -1,22 +1,9 @@
 import dynamic from 'next/dynamic';
-import { ComponentProps, Suspense } from 'react';
 import { Skeleton } from '~/components/shared';
 
-/**
- * Component used to address hydration errors
- */
-
-const LastUpdated = dynamic(() => import('./last-updated'), {
-  suspense: true,
+const DynamicLastUpdated = dynamic(() => import('./last-updated'), {
   ssr: false,
+  loading: () => <Skeleton className='h-5 w-60' />,
 });
-
-const DynamicLastUpdated = ({
-  lastUpdated,
-}: ComponentProps<typeof LastUpdated>) => (
-  <Suspense fallback={<Skeleton className='w-60 h-5' />}>
-    <LastUpdated lastUpdated={lastUpdated} />
-  </Suspense>
-);
 
 export default DynamicLastUpdated;
