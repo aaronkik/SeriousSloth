e9d471c35b3665b6deb47655af05a0789db3b2f9
Aaron <37105197+aaronkik@users.noreply.github.com>
2022-09-22 14:33:59 +0100

    Implement fetch more history times (#4)
    * Implement fetchMore history times
    
    * Update button styling

--- components/shared/button.tsx
+++ components/shared/button.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 const Button = ({ className, ...props }: ComponentProps<'button'>) => (
   <button
     className={twMerge(
-      'inline-flex select-none items-center justify-center rounded-md bg-purple-500 px-4 py-1.5 font-medium leading-4 text-neutral-100 transition-colors duration-150 focus-visible:bg-purple-700 active:bg-purple-600',
+      'inline-flex select-none items-center justify-center rounded-md bg-purple-500 px-4 py-1.5 font-medium leading-6 text-neutral-100 transition-colors duration-150 focus-visible:bg-purple-700 active:bg-purple-600',
       className
     )}
     {...props}
