53273b6dee4ca4f383a43f8fffdc853a07330b02
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2025-01-31 02:33:42 +0000

    chore(deps): update dependencies and add sharp package

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['static-cdn.jtvnw.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static-cdn.jtvnw.net',
+      },
+    ],
   },
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 module.exports = nextConfig;
