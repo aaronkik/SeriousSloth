56ea2f85e7cf171ca0c7a55c5fb169306348fca2
Aaron <37105197+aaronkik@users.noreply.github.com>
2026-04-29 14:49:37 +0100

    feat(infra): add Twitch emotes event store table

--- apps/emotes-service/infra/components/stateful.go
+++ apps/emotes-service/infra/components/stateful.go
@@ -1,13 +1,16 @@
 package components
 
 import (
+	"emotes-service/infra/stack"
+
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/dynamodb"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type StatefulComponent struct {
 	pulumi.ResourceState
-	TwitchEmotesSnapshotsTable *dynamodb.Table
+	TwitchEmotesEmotesEventStoreTable *dynamodb.Table
+	TwitchEmotesSnapshotsTable        *dynamodb.Table
 }
 
 func NewStatefulComponent(ctx *pulumi.Context, providerResource pulumi.ResourceOption) (*StatefulComponent, error) {
@@ -41,6 +44,30 @@ func NewStatefulComponent(ctx *pulumi.Context, providerResource pulumi.ResourceO
 		return nil, err
 	}
 
+	twitchEmotesEventStoreTable, err := dynamodb.NewTable(ctx, "twitch-emotes-event-store", &dynamodb.TableArgs{
+		BillingMode: pulumi.String("PAY_PER_REQUEST"),
+		HashKey:     pulumi.String("PK"),
+		RangeKey:    pulumi.String("SK"),
+		Attributes: dynamodb.TableAttributeArray{
+			&dynamodb.TableAttributeArgs{
+				Name: pulumi.String("PK"),
+				Type: pulumi.String("S"),
+			},
+			&dynamodb.TableAttributeArgs{
+				Name: pulumi.String("SK"),
+				Type: pulumi.String("S"),
+			},
+		},
+		DeletionProtectionEnabled: pulumi.BoolPtr(stack.IsProduction(ctx.Stack())),
+	},
+		pulumi.Parent(component),
+		providerResource,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	component.TwitchEmotesEmotesEventStoreTable = twitchEmotesEventStoreTable
 	component.TwitchEmotesSnapshotsTable = twitchEmotesSnapshotsTable
 
 	return component, nil
