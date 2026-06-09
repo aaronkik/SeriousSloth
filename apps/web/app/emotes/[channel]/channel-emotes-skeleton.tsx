78abc953519ab5d049d4d0aa7129f3e0819ed161
Aaron <37105197+aaronkik@users.noreply.github.com>
2026-06-09 01:24:34 +0100

    refactor(web): drop last-updated skeleton placeholder

--- apps/web/app/emotes/[channel]/channel-emotes-skeleton.tsx
+++ apps/web/app/emotes/[channel]/channel-emotes-skeleton.tsx
@@ -5,7 +5,6 @@ const ChannelEmotesSkeleton = () => (
   <>
     <div className='mb-2 flex flex-col items-center gap-2 text-center'>
       <Skeleton className='h-10 w-72 md:h-14' />
-      <Skeleton className='h-5 w-60' />
     </div>
     <EmoteTabsSkeleton />
   </>
